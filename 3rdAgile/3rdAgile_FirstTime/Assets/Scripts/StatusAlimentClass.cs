62521d2cbad8b27fbd4d0c58481e2fdebf361676
game\242j005 <ttno6018@gmail.com>
2026-07-10 13:42:23 +0900

    状態異常のenumが入っているクラスを追加しました

---
+++ 3rdAgile/3rdAgile_FirstTime/Assets/Scripts/StatusAlimentClass.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public enum StatusAliment
+{
+    SpeedDown,
+    CurseAliment,
+    StanAliment,
+    DedAliment
+}
+
+public class StatusAlimentClass : MonoBehaviour
+{
+    
+}
