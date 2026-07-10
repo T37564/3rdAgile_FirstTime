354b766afa6a34d4ef65eabc70a64d57e95cd52a
GAME\242j008 <sayassy.0106@gmail.com>
2026-07-10 16:42:16 +0900

    リベースのためコミット

---
+++ 3rdAgile/3rdAgile_FirstTime/Assets/Scripts/InGame/PlayerDeathObservation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Diagnostics;
+using Network.Player;
+
+public class PlayerDeathObservation : MonoBehaviour
+{
+    // PlayerControlleréQè∆óp
+    List<PlayerController> players =new List<PlayerController> ();
+
+    private void Start()
+    {
+        
+    }
+}
