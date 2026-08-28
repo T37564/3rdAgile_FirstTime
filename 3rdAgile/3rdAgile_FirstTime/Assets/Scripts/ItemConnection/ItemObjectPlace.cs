a528a619c0ca6ad9adbe87979269b8b1a430f020
game\242j005 <ttno6018@gmail.com>
2026-08-28 15:51:33 +0900

    リベースのためコミット。アイテムの回転を固定しました。

--- 3rdAgile/3rdAgile_FirstTime/Assets/Scripts/ItemConnection/ItemObjectPlace.cs
+++ 3rdAgile/3rdAgile_FirstTime/Assets/Scripts/ItemConnection/ItemObjectPlace.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 [System.Serializable]
 public class ItemProbability
